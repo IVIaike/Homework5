acdd6dddf6be8a6f72972ae079d41af376406b80
IVIaike <trainer.ekb@gmail.com>
2022-11-20 13:33:34 +0500

    Задача 2 выполнена

--- src/Conditions2.java
+++ src/Conditions2.java
@@ -13,5 +13,25 @@ public class Conditions2 {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
 
+        //Задача2
+        System.out.println("Задача 2");
+
+        int clientOS2 = 0; //iOs = 0, android = 1
+        int clientDeviceYear = 2014;  //2014 и ранее - облегченная версия, 2015 и позже - обычная
+
+        if (clientOS2 == 0) {
+            if (clientDeviceYear>=2015)
+            System.out.println("Установите версию приложения для iOS по ссылке");
+            else {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+        }
+        else if (clientDeviceYear>=2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        else {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+
     }
 }
